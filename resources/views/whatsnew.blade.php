449e173d61096f1d01942379e6477c65940e6ea5
Christoph Fischer <chris@toph.de>
2019-02-14 10:33:32 +0100

    Updated whatsnew

--- resources/views/whatsnew.blade.php
+++ resources/views/whatsnew.blade.php
@@ -5,6 +5,15 @@
         <h1>Neue Funktionen</h1>
         <ul>
             <li>
+                <b>14.02.2019:</b>
+                Mit "Berichte &gt; Jahresplan der Gottesdienste" kann eine Exceldatei mit einem Jahresplan aller Gottesdienste (inkl. Opfer) erzeugt werden.
+            </li>
+            <li>
+                <b>14.02.2019:</b>
+                Zu den Gottesdiensten können nun auch Opferzweck, Opferzähler, usw. angegeben werden. "Taufe" und "Abendmahl" sind Ankreuzfelder geworden, die den
+                entsprechenden Text automatisch in die Anmerkungen einfügen und z.B. in Statistiken, usw. relevant sind.
+            </li>
+            <li>
                 <b>13.02.2019:</b>
                 Mit "Berichte &gt; Prädikantenanforderung" kann das Prädikantenformular für das Dekanat automatisch erstellt werden.
             </li>
