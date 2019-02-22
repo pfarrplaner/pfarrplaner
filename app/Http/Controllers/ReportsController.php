3ed1c9fb48264b4a138029efa3745000b7aa0012
Christoph Fischer <chris@toph.de>
2019-02-22 14:14:22 +0100

    Fixed broken check if class exists

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -2,23 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\City;
-use App\Day;
-use App\Liturgy;
 use App\Reports\AbstractReport;
-use App\Service;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use PhpOffice\PhpSpreadsheet\IOFactory;
-use PhpOffice\PhpSpreadsheet\RichText\RichText;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Style\Alignment;
-use PhpOffice\PhpSpreadsheet\Style\Border;
-use PhpOffice\PhpSpreadsheet\Style\Fill;
-use PhpOffice\PhpSpreadsheet\Worksheet\PageSetup;
-use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
-use PhpOffice\PhpWord\Shared\Converter;
 
 class ReportsController extends Controller
 {
@@ -28,14 +13,16 @@ class ReportsController extends Controller
         $this->middleware('auth');
     }
 
-    public function list() {
+    public function list()
+    {
         $reports = [];
-        foreach (glob(app_path('Reports').'/*Report.php') as $file) {
+        foreach (glob(app_path('Reports') . '/*Report.php') as $file) {
             if (substr(pathinfo($file, PATHINFO_FILENAME), 0, 8) !== 'Abstract') {
-                $reportClass = 'App\\Reports\\'.pathinfo($file, PATHINFO_FILENAME);
-                if (class_exists($reportClass));
-                $report = new $reportClass();
-                $reports[$report->group][$report->title] = $report ;
+                $reportClass = 'App\\Reports\\' . pathinfo($file, PATHINFO_FILENAME);
+                if (class_exists($reportClass)) {
+                    $report = new $reportClass();
+                    $reports[$report->group][$report->title] = $report;
+                }
                 ksort($reports[$report->group]);
             }
         }
@@ -43,8 +30,9 @@ class ReportsController extends Controller
         return view('reports.list', compact('reports'));
     }
 
-    public function setup($report) {
-        $reportClass = 'App\\Reports\\'.ucfirst($report).'Report';
+    public function setup($report)
+    {
+        $reportClass = 'App\\Reports\\' . ucfirst($report) . 'Report';
         if (class_exists($reportClass)) {
             /** @var AbstractReport $report */
             $report = new $reportClass();
@@ -54,8 +42,9 @@ class ReportsController extends Controller
         }
     }
 
-    public function render(Request $request, $report) {
-        $reportClass = 'App\\Reports\\'.ucfirst($report).'Report';
+    public function render(Request $request, $report)
+    {
+        $reportClass = 'App\\Reports\\' . ucfirst($report) . 'Report';
         if (class_exists($reportClass)) {
             /** @var AbstractReport $report */
             $report = new $reportClass();
