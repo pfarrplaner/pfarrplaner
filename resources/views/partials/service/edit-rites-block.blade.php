374342975d8d27ad8c46785c7b473df5e11ff573
Christoph Fischer <chris@toph.de>
2019-05-13 16:51:25 +0200

    [FIX] Fixed edit route for rites in home screen

--- resources/views/partials/service/edit-rites-block.blade.php
+++ resources/views/partials/service/edit-rites-block.blade.php
@@ -1,6 +1,6 @@
     <a class="btn btn-sm btn-secondary" href="{{ route('calendar', ['month' => $service->day->date->format('m'), 'year' => $service->day->date->format('Y')]) }}" title="Im Kalender ansehen"><span class="fa fa-calendar"></span></a>
-@if(null !== $service)
-@can('update', $service)
-    <a class="btn btn-sm btn-primary" href="{{route('services.edit', $service->id).'#rites'}}" title="Bearbeiten"><span class="fa fa-edit"></span></a>
-@endcan
-@endif
+    @if(null !== $service)
+        @can('update', $service)
+            <a class="btn btn-sm btn-primary" href="{{route('services.edit', ['service' => $service->id, 'tab' => 'rites'])}}" title="Bearbeiten"><span class="fa fa-edit"></span></a>
+        @endcan
+    @endif
