e7a106b563764522eefa7b85b0c625303489b8c0
Christoph Fischer <chris@toph.de>
2019-05-14 20:04:21 +0200

    [FIX] Crash on new time format without seconds

--- resources/views/ical/ical.blade.php
+++ resources/views/ical/ical.blade.php
@@ -1,6 +1,6 @@
 BEGIN:VCALENDAR
 VERSION:2.0
-PRODID:{{ env('APP_URL') }}ical/{{ $action }}/{{ $key }}/{{ $token }}
+PRODID:{{ env('APP_URL') }}@if(null !== $token)ical/{{ $action }}/{{ $key }}/{{ $token }}@endif
 METHOD:PUBLISH
 @foreach($services as $service)BEGIN:VEVENT
 UID:{{ $service->id }}{{ '@' }}{{ parse_url(env('APP_URL'), PHP_URL_HOST) }}
@@ -11,8 +11,8 @@ DESCRIPTION: {{ wordwrap ($service->description, 62, "\r\n  ") }}
 @endif
 
 CLASS:PUBLIC
-DTSTART:{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $service->day->date->format('Y-m-d').' '.$service->time, 'Europe/Berlin')->setTimezone('UTC')->format('Ymd\THis\Z') }}
-DTEND:{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $service->day->date->format('Y-m-d').' '.$service->time, 'Europe/Berlin')->addHour(1)->setTimezone('UTC')->format('Ymd\THis\Z') }}
+DTSTART:{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $service->day->date->format('Y-m-d').' '.$service->time.':00', 'Europe/Berlin')->setTimezone('UTC')->format('Ymd\THis\Z') }}
+DTEND:{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $service->day->date->format('Y-m-d').' '.$service->time.':00', 'Europe/Berlin')->addHour(1)->setTimezone('UTC')->format('Ymd\THis\Z') }}
 DTSTAMP:{{ $service->updated_at->setTimezone('UTC')->format('Ymd\THis\Z') }}
 END:VEVENT
 @endforeach
