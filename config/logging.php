75d002b2343149949c69801e1d57735344834326
Christoph Fischer <chris@toph.de>
2019-05-17 15:34:35 +0200

    [FIX] Logging failed on file permissions

--- config/logging.php
+++ config/logging.php
@@ -47,7 +47,7 @@ return [
 
         'daily' => [
             'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path('logs/'.php_sapi_name().'/laravel.log'),
             'level' => 'debug',
             'days' => 14,
             'permission' => 0664,
