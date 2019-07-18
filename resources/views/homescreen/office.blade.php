c07d957d014a93a043bf6e07d225d3f015bf98d0
Christoph Fischer <chris@toph.de>
2019-07-18 11:27:37 +0200

    [FIX] Remove "Show in calendar" for undated baptims

--- resources/views/homescreen/office.blade.php
+++ resources/views/homescreen/office.blade.php
@@ -137,7 +137,6 @@
                             <tr class="">
                                 @include('partials.baptism.details', ['baptism' => $baptism])
                                 <td>
-                                    <a class="btn btn-sm btn-secondary" href="{{ route('calendar', ['month' => $service->day->date->format('m'), 'year' => $service->day->date->format('Y')]) }}" title="Im Kalender ansehen"><span class="fa fa-calendar"></span></a>
                                     <a class="btn btn-sm btn-primary" href="{{route('baptisms.edit', $baptism->id)}}" title="Bearbeiten"><span class="fa fa-edit"></span></a>
                                     <form action="{{ route('baptisms.destroy', $baptism->id) }}" method="post">
                                         @method('DELETE')
