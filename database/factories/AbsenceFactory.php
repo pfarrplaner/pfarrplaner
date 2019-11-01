109c68721ca955bf96c6682b2c55ce09976a34f7
Christoph Fischer <chris@toph.de>
2019-11-01 10:44:52 +0100

    Add missing files to VCS

---
+++ database/factories/AbsenceFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Absence::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/ReplacementFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Replacement::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
