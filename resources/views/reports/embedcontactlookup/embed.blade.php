26d1d175889a0180d2fde023bc1f223ba47b1c26
Christoph Fischer <chris@toph.de>
2019-11-04 10:02:07 +0100

    Add parish users to EmbedContactLookupReport

--- resources/views/reports/embedcontactlookup/embed.blade.php
+++ resources/views/reports/embedcontactlookup/embed.blade.php
@@ -1,51 +1,70 @@
 @if (!$parish)
-<div id="{{ $randomId }}" class="row ctype-textbox listtype-none showmobdesk-0">
-    <div id="c1050561" class="col s12 bullme ">
-        <div class="card-panel default">
-
-            <p class="bodytext"><b>Ihr Ansprechpartner</b></p>
-        <p class="bodytext">Damit wir einen Ansprechpartner für Sie ermitteln können, geben Sie bitte hier Ihre Adresse ein:</p>
-            <div>
-                <label for="street">Straße:</label>
-                <select id="street">
-                    @foreach ($streets as $street)
-                        <option>{{ $street }}</option>
-                    @endforeach
-                </select><br />
-                <label for="number">Hausnummer</label>
-                <input id="number" value="" type="text"/>
-                <button id="btnFind">Ansprechpartner finden</button>
-            </div>
-        </div>
-    </div>
-</div>
-<script src="https://code.jquery.com/jquery-3.3.1.min.js" type="text/javascript"></script><script defer>
-    $(document).ready(function(){
-        $('#btnFind').click(function(){
-            var url = '{{ $url }}&street='+$('#street').val()+'&number='+$('#number').val();
-            if (($('#street').val() != '') && ($('number').val() != '')) {
-                $('#{{ $randomId }}').html('Bitte warten, Daten werden übertragen...');
-                fetch(url)
-                    .then((res) => {return res.text();})
-                    .then((data) => {
-                        $('#{{ $randomId }}').html(data);
-                    });
-            }
-        })
-
-    });
-</script>
-@else
     <div id="{{ $randomId }}" class="row ctype-textbox listtype-none showmobdesk-0">
         <div id="c1050561" class="col s12 bullme ">
             <div class="card-panel default">
 
                 <p class="bodytext"><b>Ihr Ansprechpartner</b></p>
-                <p class="bodytext"><b>{{ $parish->name }}</b><br />
-                    {{ $parish->address }}<br />
-                    {{ $parish->zip }} {{ $parish->city }}<br />
-                    Fon {{ $parish->phone }} | E-Mail <a href="mailto:{{ $parish->email }}">{{ $parish->email }}</a>
-                </p>
+                <p class="bodytext">Damit wir einen Ansprechpartner für Sie ermitteln können, geben Sie bitte hier Ihre
+                    Adresse ein:</p>
+                <div>
+                    <label for="street">Straße:</label>
+                    <select id="street">
+                        @foreach ($streets as $street)
+                            <option>{{ $street }}</option>
+                        @endforeach
+                    </select><br/>
+                    <label for="number">Hausnummer</label>
+                    <input id="number" value="" type="text"/>
+                    <button id="btnFind">Ansprechpartner finden</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" type="text/javascript"></script>
+    <script defer>
+        $(document).ready(function () {
+            $('#btnFind').click(function () {
+                var url = '{{ $url }}&street=' + $('#street').val() + '&number=' + $('#number').val();
+                if (($('#street').val() != '') && ($('number').val() != '')) {
+                    $('#{{ $randomId }}').html('Bitte warten, Daten werden übertragen...');
+                    fetch(url)
+                        .then((res) => {
+                            return res.text();
+                        })
+                        .then((data) => {
+                            $('#{{ $randomId }}').html(data);
+                        });
+                }
+            })
+
+        });
+    </script>
+@else
+    <div id="{{ $randomId }}" class="row ctype-textbox listtype-none showmobdesk-0">
+        <div id="c1050561" class="col s12 bullme ">
+            <div class="card-panel default">
+
+                <p class="bodytext"><b>{{ \App\Tools\StringTool::pluralString(count($parish->users), 'Ihr', 'Ihre') }}
+                        Ansprechpartner</b></p><br />
+                @foreach ($parish->users as $user)
+                    <div>
+                        @if ($user->image)
+                            <div style="float: left; width: 90px;">
+                                <img width="80px" src="{{ url('storage/'.$user->image) }}"/>
+                            </div>
+                        @endif
+                        <div style="float:left;">
+                            <p class="bodytext"><b>{{ $user->fullName(true) }}</b><br/>
+                                {{ $parish->name }}<br/>
+                                {{ $parish->address }}<br/>
+                                {{ $parish->zip }} {{ $parish->city }}<br/>
+                                Fon {{ $parish->phone }}<br />E-Mail <a
+                                        href="mailto:{{ $parish->email }}">{{ $parish->email }}</a>
+                            </p>
+                        </div>
+                    </div>
+                    <div style="clear: both;"></div>
+                @endforeach
             </div>
         </div>
     </div>
