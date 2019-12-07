ebc8236145267a4ecc53c97a63fbc169d0598f2c
Christoph Fischer <chris@toph.de>
2019-12-07 15:25:29 +0100

    Include slave body class

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,7 +39,7 @@
     <meta name="msapplication-config" content="/img/favicons/browserconfig.xml">
     <meta name="theme-color" content="#ffffff">
 </head>
-<body class="hold-transition sidebar-mini sidebar-collapse {{ strtolower(str_replace('.', '-', Request::route()->getName())) }}">
+<body class="hold-transition sidebar-mini sidebar-collapse {{ strtolower(str_replace('.', '-', Request::route()->getName())) }} @if(isset($slave)) slave @endif">
 
 <div class="wrapper">
     <!-- Navbar -->
