99fdf5dba5dff3d9108ae04451c80fb1d2436199
Christoph Fischer <chris@toph.de>
2019-12-13 09:31:35 +0100

    [FIX] select threw exception on string value

--- resources/views/partials/form/select.blade.php
+++ resources/views/partials/form/select.blade.php
@@ -3,10 +3,11 @@
     <select class="form-control @if(isset($class)){{ $class }} @endif" name="{{ $name }}" @if(false !== strpos($name, '[]')) multiple @endif @if(isset($enabled) && (!$enabled)) disabled @endcannot @if(isset($id)) id="{{ $id }}_input" @endif>
         @foreach ($items as $item)
             @if(isset($empty) && $empty)<option></option>@endif
-                @if(strpos($name, '[]') !== false)
+                @if((strpos($name, '[]') !== false))
                     <option @if(isset($item->id))value="{{ $item->id }}" @endif @if(isset($value) && ($value->contains($item))) selected @endif>@if(isset($item->name)){{ $item->name }}@else{{$item}}@endif</option>
                 @else
-                    <option @if(isset($item->id))value="{{ $item->id }}" @endif @if(isset($value) && ($value == $item->id)) selected @endif>@if(isset($item->name)){{ $item->name }}@else{{$item}}@endif</option>
+                    <?php $checkValue = is_object($item) ? $item->id : $item; ?>
+                    <option @if(is_object($item) && isset($item->id))value="{{ $item->id }}" @endif @if(isset($value) && ($value == $checkValue)) selected @endif>@if(isset($item->name)){{ $item->name }}@else{{$item}}@endif</option>
                 @endif
         @endforeach
     </select>
