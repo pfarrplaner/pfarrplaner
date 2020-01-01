1a1e3bd64345727ecd1e770b83ae2a4b6ca7e8ed
Christoph Fischer <chris@toph.de>
2020-01-01 19:45:21 +0100

    Add testing for Day model

---
+++ tests/Feature/DayTest.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\City;
+use App\Day;
+use App\Http\Middleware\Authenticate;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class DayTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->withoutMiddleware([Authenticate::class]);
+    }
+
+
+    /**
+     * Test if a day can be created
+     * @return void
+     * @test
+     */
+    public function testDayCanBeCreated()
+    {
+        $this->withoutExceptionHandling();
+        $data = factory(Day::class)->raw(['day_type' => 0]);
+        $data['date'] = $data['date']->format('d.m.Y');
+        $response = $this->post(route('days.store'), $data);
+
+        $response->assertStatus(302);
+        $this->assertCount(1, Day::all());
+    }
+
+    /**
+     * Test if a day of type DAY_LIMITED can be created
+     * @return void
+     * @test
+     */
+    public function testLimitedDayCanBeCreated()
+    {
+        $this->withoutExceptionHandling();
+        $data = factory(Day::class)->raw(['day_type' => 1]);
+        $data['date'] = $data['date']->format('d.m.Y');
+        $data['cities'] = [factory(City::class)->create()->id];
+        $response = $this->post(route('days.store'), $data);
+
+        $response->assertStatus(302);
+        $this->assertCount(1, Day::all());
+    }
+
+    /**
+     * Test if a day of type DAY_LIMITED cannot be created without cities
+     * @return void
+     * @test
+     */
+    public function testLimitedDayCannotBeCreatedWithoutCities()
+    {
+        $data = factory(Day::class)->raw(['day_type' => 1]);
+        $data['date'] = $data['date']->format('d.m.Y');
+        $response = $this->post(route('days.store'), $data);
+
+        $response->assertSessionHasErrors('cities');
+        $this->assertCount(0, Day::all());
+    }
+
+    /**
+     * Test if a day can't be created without date
+     * @return void
+     * @test
+     */
+    public function testDayCannotBeCreatedWithoutDate()
+    {
+        $response = $this->post(route('days.store'), factory(Day::class)->raw(['date' => null]));
+        $response->assertSessionHasErrors('date');
+        $this->assertCount(0, Day::all());
+    }
+
+    /**
+     * Test if a day can't be created without correct date format
+     * @return void
+     * @test
+     */
+    public function testDayCannotBeCreatedWithoutCorrectDateFormat()
+    {
+        $response = $this->post(route('days.store'), factory(Day::class)->raw(['date' => str_random(5)]));
+        $response->assertSessionHasErrors('date');
+        $this->assertCount(0, Day::all());
+    }
+
+    /**
+     * Test if a day can't be created with an invalid day_type
+     * @return void
+     * @test
+     */
+    public function testDayCannotBeCreatedWithInvalidDayType()
+    {
+        $response = $this->post(route('days.store'), factory(Day::class)->raw(['day_type' => 5, 'date' => '01.01.1990']));
+        $response->assertSessionHasErrors('day_type');
+        $this->assertCount(0, Day::all());
+    }
+
+    /**
+     * Test if a day can be updated with an invalid day_type
+     * @return void
+     * @test
+     */
+    public function testDayCanBeUpdated()
+    {
+        $this->withoutExceptionHandling();
+        $day = factory(Day::class)->create(['day_type' => 0]);
+        $response = $this->patch(route('days.update', $day->id), [
+            'date' => '01.01.1990',
+            'name' => 'test',
+        ]);
+        $response->assertStatus(302);
+        $this->assertEquals('test', Day::find($day->id)->name);
+    }
+
+    /**
+     * Test if a day can be updated with more cities
+     * @return void
+     * @test
+     */
+    public function testDayCanBeUpdatedWithMoreCities()
+    {
+        $this->withoutExceptionHandling();
+        $city1 = factory(City::class)->create()->id;
+        $city2 = factory(City::class)->create()->id;
+        $data = factory(Day::class)->raw(['day_type' => 1]);
+        $day = Day::create($data);
+        $day->cities()->attach($city1);
+        $response = $this->patch(route('days.update', $day->id), [
+            'date' => '01.01.1990',
+            'name' => 'test',
+            'cities' => [$city1, $city2]
+        ]);
+        $response->assertStatus(302);
+
+        $day = Day::find($day->id);
+        $this->assertTrue($day->cities->pluck('id')->contains($city2));
+    }
+
+    /**
+     * Test if a limited day is deleted when no more cities are attached
+     * @return void
+     * @test
+     */
+    public function testLimitedDayIsDeletedWhenNoMoreCitiesAreAttached()
+    {
+        $this->withoutExceptionHandling();
+        $city1 = factory(City::class)->create()->id;
+        $city2 = factory(City::class)->create()->id;
+        $data = factory(Day::class)->raw(['day_type' => 1]);
+        $day = Day::create($data);
+        $day->cities()->attach([$city1,$city2]);
+        $response = $this->patch(route('days.update', $day->id), [
+            'date' => '01.01.1990',
+            'day_type' => 1,
+            'name' => 'test',
+            'cities' => []
+        ]);
+        $response->assertStatus(302);
+        $this->assertCount(0, Day::all());
+    }
+
+
+    /**
+     * Test if a day can be deleted
+     * @return void
+     * @test
+     */
+    public function testDayCanBeDeleted()
+    {
+        $this->withoutExceptionHandling();
+        $day = factory(Day::class)->create(['day_type' => 0]);
+        $response = $this->delete(route('days.destroy', $day->id));
+        $response->assertStatus(302);
+        $this->assertCount(0, Day::all());
+    }
+
+}
