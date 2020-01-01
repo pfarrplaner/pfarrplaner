c6322a28cb2d9e315fad20e4943181a66e76729a
Christoph Fischer <chris@toph.de>
2020-01-01 19:45:41 +0100

    Add factory for Day model

---
+++ database/factories/DayFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(\App\Day::class, function (Faker $faker) {
+    return [
+        'date' => new Carbon\Carbon($faker->date()),
+        'day_type' => $faker->randomElement([\App\Day::DAY_TYPE_DEFAULT, \App\Day::DAY_TYPE_LIMITED]),
+        'name' => $faker->randomAscii,
+        'description' => $faker->randomAscii,
+    ];
+});
