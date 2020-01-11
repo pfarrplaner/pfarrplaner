713bfe3462167a886dfa0dc20a5c18203c2b769d
Christoph Fischer <chris@toph.de>
2020-01-11 16:42:44 +0100

    Stricter access checking in DownloadController

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Attachment;
 use App\Helpers\FileHelper;
+use App\Service;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
 class DownloadController extends Controller
@@ -21,6 +23,14 @@ class DownloadController extends Controller
     }
 
     public function attachment(Request $request, Attachment $attachment, $prettyName = '') {
+        if (get_class($attachment->attachable) == Service::class) {
+            if (!Auth::user()->can('gd-bearbeiten')) abort(403);
+            if (!Auth::user()->writableCities->contains($attachment->attachable->city)) abort(403);
+        } else {
+            if (!Auth::user()->can('gd-kasualien-bearbeiten')) abort(403);
+            if (!Auth::user()->writableCities->contains($attachment->attachable->service->city)) abort(403);
+        }
+
         $prettyName = $prettyName ?  FileHelper::normalizeFilename($prettyName) :
             (FileHelper::normalizeFilename($attachment->title) ?? $attachment->id).'.'.pathinfo($attachment->file, PATHINFO_EXTENSION) ;
         return Storage::download($attachment->file, $prettyName);
