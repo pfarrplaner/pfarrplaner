fe2add6182ba6397180bedb4a4631a9703905c5a
Christoph Fischer <chris@toph.de>
2020-01-21 17:50:50 +0100

    [FIX] Carbon throws error due to new time format in service model

--- app/Reports/BillBoardReport.php
+++ app/Reports/BillBoardReport.php
@@ -212,7 +212,7 @@ class BillBoardReport extends AbstractWordDocumentReport
             }
 
             $this->renderParagraph(self::DEFAULT, [
-                [Carbon::createFromFormat('Y-m-d H:i:s', $event->day->date->format('Y-m-d').' '.$event->time)->formatLocalized('%H.%M Uhr')."\t", []],
+                [Carbon::createFromFormat('Y-m-d H:i', $event->day->date->format('Y-m-d').' '.$event->time)->formatLocalized('%H.%M Uhr')."\t", []],
                 ['Gottesdienst'.$description, self::BOLD],
                 [' ('.$event->locationText().')', []],
                 ["\t" . $event->participantsText('P'), self::BOLD]
