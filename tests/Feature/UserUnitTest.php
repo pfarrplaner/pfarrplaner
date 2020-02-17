e9defad82a9b533b926fd0b3537dc729bbeee8da
Christoph Fischer <chris@toph.de>
2020-02-17 08:45:52 +0100

    Initial unit testing for User model

---
+++ tests/Feature/UserUnitTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\HomeScreens\PastorHomeScreen;
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserUnitTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    /**
+     * Test if getting a HomeScreenObject works
+     *
+     * @return void
+     */
+    public function testGetHomeScreen()
+    {
+        $user = factory(User::class)->create();
+        $this->assertNull($user->getHomeScreen());
+        $user->setSetting('homeScreen', 'pastor');
+        $this->assertInstanceOf(PastorHomeScreen::class, $user->getHomeScreen());
+    }
+}
