2a918cd0ccb0e8fc43c5bff87b07c6730af5861b
Christoph Fischer <chris@toph.de>
2020-02-17 11:05:39 +0100

    Better error handling

--- resources/views/partials/service/edit-rites-block.blade.php
+++ resources/views/partials/service/edit-rites-block.blade.php
@@ -1,6 +1,8 @@
-    <a class="btn btn-sm btn-secondary" href="{{ route('calendar', ['month' => $service->day->date->format('m'), 'year' => $service->day->date->format('Y')]) }}" title="Im Kalender ansehen"><span class="fa fa-calendar"></span></a>
+@if(isset($service))
     @if(null !== $service)
+        <a class="btn btn-sm btn-secondary" href="{{ route('calendar', ['month' => $service->day->date->format('m'), 'year' => $service->day->date->format('Y')]) }}" title="Im Kalender ansehen"><span class="fa fa-calendar"></span></a>
         @can('update', $service)
             <a class="btn btn-sm btn-primary" href="{{route('services.edit', ['service' => $service->id, 'tab' => 'rites'])}}?back=/home" title="Gottesdienst bearbeiten"><span class="fa fa-edit"></span></a>
         @endcan
     @endif
+@endif
