d3f38bf5eb8195fcc876f2a7db00b482113d3f52
Christoph Fischer <chris@toph.de>
2020-05-12 14:57:16 +0200

    [FIX] Multiple empty options in select.blade.php

--- resources/views/partials/form/select.blade.php
+++ resources/views/partials/form/select.blade.php
@@ -1,8 +1,8 @@
 <div class="form-group" @if(isset($id)) id="{{ $id }}" @endif>
     @if($label)<label for="{{ $name }}">{{ $label }}</label>@endif
     <select class="form-control @if(isset($class)){{ $class }} @endif" name="{{ $name }}" @if(false !== strpos($name, '[]')) multiple @endif @if(isset($enabled) && (!$enabled)) disabled @endcannot @if(isset($id)) id="{{ $id }}_input" @endif>
+        @if(isset($empty) && $empty)<option></option>@endif
         @foreach ($items as $item)
-            @if(isset($empty) && $empty)<option></option>@endif
                 @if((strpos($name, '[]') !== false))
                     <option @if(isset($item->id))value="{{ $item->id }}" @endif @if(isset($value) && ($value->contains($item))) selected @endif>@if(isset($item->name)){{ $item->name }}@else{{$item}}@endif</option>
                 @else
