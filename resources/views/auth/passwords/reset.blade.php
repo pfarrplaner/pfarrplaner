2f020bfda3c1c1d830aea814e35dba111685156f
Christoph Fischer <chris@toph.de>
2020-06-23 14:49:46 +0200

    Fix typo

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -14,7 +14,7 @@
                         <input type="hidden" name="token" value="{{ $token }}">
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">E-Mailaddresse</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">E-Mailadresse</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email ?? old('email') }}" required autofocus>
