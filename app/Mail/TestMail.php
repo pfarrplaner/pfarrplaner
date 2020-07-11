7f10d2948a8fb421e86b34142490315f7fb3db6b
Christoph Fischer <chris@toph.de>
2020-07-11 15:36:10 +0200

    Add missing file TestMail.php to Git

---
+++ app/Mail/TestMail.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class TestMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Test der E-Mailfunktion')->replyTo('chris@toph.de')->view('mail.test.test');
+    }
+}
