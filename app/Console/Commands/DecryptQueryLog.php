6e2aab7ba17d7780f5b3ca3077d37c8fa91118e4
Christoph Fischer <chris@toph.de>
2020-08-19 19:04:52 +0200

    Implement query logging to create a simple backup of changes since the nightly backup

---
+++ app/Console/Commands/DecryptQueryLog.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\QueryLog;
+use Illuminate\Console\Command;
+
+/**
+ * Class DecryptQueryLog
+ * @package App\Console\Commands
+ */
+class DecryptQueryLog extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'query:list';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'List all queries executed against the database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $queries = QueryLog::all();
+        foreach ($queries as $query) {
+            $this->line($query['query']);
+        }
+    }
+}
