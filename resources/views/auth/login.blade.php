707a0495af5c4b96fb12b18cacca5ae654705d61
Christoph Fischer <chris@toph.de>
2020-08-20 12:51:34 +0200

    Form refactoring

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,81 +4,34 @@
 
 @section('content')
     @component('components.container')
-        <div class="card">
-            <div class="card-header">Anmelden</div>
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+            @component('components.ui.card')
+                @slot('cardHeader')
+                    Anmelden
+                @endslot
+                @slot('cardFooter')
+                    <button type="submit" class="btn btn-primary">
+                        Anmelden
+                    </button>
 
-            <div class="card-body">
-                <form method="POST" action="{{ route('login') }}">
-                    @csrf
-
-                    <div class="form-group row">
-                        <label for="email" class="col-md-4 col-form-label text-md-right">E-Mailadresse</label>
-
-                        <div class="col-md-6">
-                            <input id="email" type="email"
-                                   class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email"
-                                   value="{{ old('email') }}" required autofocus>
-
-                            @if ($errors->has('email'))
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                            @endif
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label for="password" class="col-md-4 col-form-label text-md-right">Passwort</label>
-
-                        <div class="col-md-6">
-                            <input id="password" type="password"
-                                   class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
-                                   name="password" required>
-
-                            @if ($errors->has('password'))
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                            @endif
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <div class="col-md-6 offset-md-4">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" name="remember"
-                                       id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                <label class="form-check-label" for="remember">
-                                    Angemeldet bleiben
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="form-group row mb-0">
-                        <div class="col-md-8 offset-md-4">
-                            <button type="submit" class="btn btn-primary">
-                                Anmelden
-                            </button>
-
-                            @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Passwort vergessen?
-                                </a>
-                            @endif
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
+                    @if (Route::has('password.request'))
+                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                            Passwort vergessen?
+                        </a>
+                    @endif
+                @endslot
+                    @input(['name' => 'email', 'label' => 'E-Mailadresse', 'value' => old('email'), 'type' => 'email', 'id' => 'email'])
+                    @input(['name' => 'password', 'label' => 'Passwort', 'type' => 'password'])
+                    @checkbox(['name' => 'remember', 'label' => 'Angemeldet bleiben', 'value' => old('remember')])
+            @endcomponent
     @endcomponent
 @endsection
 
 @section('scripts')
     <script>
-        $(document).ready(function(){
-            $('#email').focus();
+        $(document).ready(function () {
+            $('#email_input').focus();
         });
     </script>
 @endsection
