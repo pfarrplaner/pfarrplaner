67877a18f6f509822b2054b5c109367a527e2a1c
Christoph Fischer <chris@toph.de>
2020-10-01 16:10:25 +0200

    Form refactoring

--- resources/views/locations/create.blade.php
+++ resources/views/locations/create.blade.php
@@ -4,51 +4,18 @@
 
 @section('content')
     <form method="post" action="{{ route('locations.store') }}">
+        @csrf
         @component('components.ui.card')
             @slot('cardFooter')
                 <button type="submit" class="btn btn-primary">Hinzufügen</button>
             @endslot
-            <div class="form-group">
-                @csrf
-                <label for="name">Name:</label>
-                <input type="text" class="form-control" id="name" name="name"/>
-            </div>
-            <div class="form-group">
-                <label for="location_id">Kirchengemeinde:</label>
-                <select class="form-control" name="city_id">
-                    @foreach ($cities->all() as $city)
-                        <option value="{{ $city->id }}">{{ $city->name }}</option>
-                    @endforeach
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="default_time">Gottesdienst um:</label>
-                <input type="text" class="form-control" id="fldDefaultTime" name="default_time"/>
-            </div>
-            <div class="form-group">
-                <label for="cc_default_location">Wenn parallel Kinderkirche stattfindet, dann normalerweise
-                    hier:</label>
-                <input type="text" class="form-control" id="cc_default_location" name="cc_default_location"/>
-            </div>
-            <div class="form-group">
-                <label for="alternate_location_id">Alternativer Ort, wenn hier kein Gottesdienst stattfindet:</label>
-                <select class="form-control fancy-selectize" name="alternate_location_id">
-                    <option></option>
-                    @foreach($alternateLocations as $alternateLocation)
-                        <option value="{{ $alternateLocation->id }}">{{ $alternateLocation->name }}</option>
-                    @endforeach
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="at_text">Ortsangabe, wenn ein Gottesdienst hier stattfindet:</label>
-                <input type="text" class="form-control" id="at_text" name="at_text"
-                       placeholder="z.B.: in der Peterskirche; auf dem Friedhof; im Gemeindezentrum Stiegel"/>
-            </div>
-            <div class="form-group">
-                <label for="general_location_name">Allgemeine Ortsangabe:</label>
-                <input type="text" class="form-control" id="general_location_name" name="general_location_name"
-                       placeholder="z.B.: in Tailfingen"/>
-            </div>
+            @input(['name' => 'name', 'label' => 'Name'])
+            @select(['name' => 'city_id', 'label' => 'Kirchengemeinde', 'items' => $cities])
+            @input(['name' => 'default_time', 'label' => 'Gottesdienst um'])
+            @input(['name' => 'cc_default_location', 'label' => 'Wenn parallel Kinderkirche stattfindet, dann normalerweise hier'])
+            @select(['name' => 'alternate_location_id', 'label' => 'Wenn parallel Kinderkirche stattfindet, dann normalerweise hier', 'items' => $alternateLocations, 'empty' => true])
+            @input(['name' => 'at_text', 'label' => 'Ortsangabe, wenn ein Gottesdienst hier stattfindet', 'placeholder' => 'Ortsangabe, wenn ein Gottesdienst hier stattfindet'])
+            @input(['name' => 'general_location_name', 'label' => 'Allgemeine Ortsangabe', 'placeholder' => 'z.B.: in Tailfingen'])
         @endcomponent
     </form>
 @endsection
