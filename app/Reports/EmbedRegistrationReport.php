7d27f4038b3025fbbdc980b208c882d25a0c1bcb
Christoph Fischer <chris@toph.de>
2020-11-02 14:15:19 +0100

    EmbedRegistrationReport

---
+++ app/Reports/EmbedRegistrationReport.php
@@ -0,0 +1,190 @@
+<?php
+/**
+ * Pfarrplaner
+ *
+ * @package Pfarrplaner
+ * @author Christoph Fischer <chris@toph.de>
+ * @copyright (c) 2020 Christoph Fischer, https://christoph-fischer.org
+ * @license https://www.gnu.org/licenses/gpl-3.0.txt GPL 3.0 or later
+ * @link https://github.com/potofcoffee/pfarrplaner
+ * @version git: $Id$
+ *
+ * Sponsored by: Evangelischer Kirchenbezirk Balingen, https://www.kirchenbezirk-balingen.de
+ *
+ * Pfarrplaner is based on the Laravel framework (https://laravel.com).
+ * This file may contain code created by Laravel's scaffolding functions.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: Christoph Fischer
+ * Date: 02.11.2019
+ * Time: 12:30
+ */
+
+namespace App\Reports;
+
+
+use App\Booking;
+use App\Service;
+use Carbon\Carbon;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\MessageBag;
+use Illuminate\View\View;
+
+/**
+ * Class EmbedContactLookupReport
+ * @package App\Reports
+ */
+class EmbedRegistrationReport extends AbstractEmbedReport
+{
+
+    /**
+     * @var string
+     */
+    public $title = 'Anmeldung zu Gottesdiensten';
+    /**
+     * @var string
+     */
+    public $group = 'Website (Gemeindebaukasten)';
+    /**
+     * @var string
+     */
+    public $description = 'Erzeugt HTML-Code für die Einbindung einer Anmeldebox für Gottesdienste';
+    /**
+     * @var string
+     */
+    public $icon = 'fa fa-file-code';
+
+    /**
+     * @return Application|Factory|View
+     */
+    public function setup()
+    {
+        $cities = Auth::user()->cities;
+        return $this->renderSetupView(compact('cities'));
+    }
+
+    /**
+     * @param Request $request
+     * @return Application|Factory|View|string
+     */
+    public function render(Request $request)
+    {
+        $request->validate(
+            [
+                'cors-origin' => 'required|url',
+                'includeCities' => 'required',
+                'includeCities.*' => 'required|exists:cities,id',
+            ]
+        );
+        $cities = join(',', $request->get('includeCities'));
+        $corsOrigin = $request->get('cors-origin');
+        $report = $this->getKey();
+
+        $url = route('report.embed', compact('report', 'cities', 'corsOrigin'));
+        $randomId = uniqid();
+
+        return $this->renderView('render', compact('url', 'randomId'));
+    }
+
+
+    /**
+     * @param Request $request
+     * @return Response|Application|Factory|View|string
+     */
+    public function embed(Request $request)
+    {
+        $cities = $request->get('cities', []);
+        if (!is_array($cities)) {
+            $cities = explode(',', $cities);
+        }
+
+        $errors = new MessageBag();
+        $success = new MessageBag();
+
+        if ($request->has('name')) {
+            $data = Validator::make(
+                $request->all(),
+                [
+                    'name' => 'required|string',
+                    'first_name' => 'required|string',
+                    'contact' => 'required|string',
+                    'number' => 'required|int|min:1',
+                    'service' => 'required|int:exists:services,id'
+                ]
+            );
+            $errors = $data->errors();
+            $data = $data->getData();
+            if (count($errors) == 0) {
+                $service = Service::find($data['service']);
+                $test = $service->getSeatFinder()->find($data['number']);
+                if ($test) {
+                    $data['service_id'] = $data['service'];
+                    $data['code'] = Booking::createCode();
+                    $booking = Booking::create($data);
+                    $message = (($data['number'] == 1) ? 'Ihr Sitzplatz wurde ' : $data['number'].' Sitzplätze wurden ')
+                        .'erfolgreich reserviert. Ihr Bestätigungscode: '.strtoupper($data['code']).'. Am Eingang zum Gottesdienst erfahren Sie, '
+                        .'wo genau Sie sitzen.';
+                    $success->add('success', $message);
+                } else {
+                    $message = ($data['number'] == 1 ? 'Leider konnte in diesem Gottesdienst kein Sitzplatz reserviert werden.' : 'Leider konnten in diesem Gottesdiens tkeine ' . $data['number'] . ' zusammenhängenden Sitzplätze reserviert werden.');
+                    $errors->add('sorry', $message);
+                }
+            }
+        }
+
+        $tmpServices = Service::where('needs_reservations', 1)
+            ->select(['services.*', 'days.date'])
+            ->join('days', 'days.id', '=', 'day_id')
+            ->whereIn('city_id', $cities)
+            ->whereHas(
+                'day',
+                function ($query) {
+                    $query->where('date', '>=', Carbon::now());
+                }
+            )
+            ->orderBy('days.date', 'ASC')
+            ->orderBy('time', 'ASC')
+            ->get();
+
+        $services = [];
+        foreach ($tmpServices as $service) {
+            $services[$service->day->date->format('Ymd')][] = $service;
+        }
+
+
+        $corsOrigin = $request->get('corsOrigin', '');
+        $report = $this->getKey();
+        $url = route('report.embed', compact('report', 'corsOrigin'));
+
+        $randomId = uniqid();
+        /** @var Response $response */
+        $response = response()
+            ->view(
+                $this->getViewName('embed'),
+                compact('randomId', 'services', 'url', 'cities', 'errors', 'success')
+            );
+
+
+        return $response;
+    }
+}
---
+++ resources/views/reports/embedregistration/embed.blade.php
@@ -0,0 +1,112 @@
+<div id="{{ $randomId }}">
+    <div id="{{ $randomId }}_form" class="row ctype-textbox listtype-none showmobdesk-0">
+        @foreach($errors->all() as $error)
+            <div
+                style="border-radius: 3px; margin: 3px 10px; padding: 3px; background-color:red; color:white;">{{ $error }}</div>
+        @endforeach
+        @foreach($success->all() as $message)
+            <div
+                style="border-radius: 3px; margin: 3px 10px; padding: 3px; background-color:green; color:white;">{{ $message }}</div>
+        @endforeach
+        <div id="{{ $randomId }}" class="col s12 bullme ">
+            @foreach ($services as $dayServices)
+                <h2 style="margin-bottom: 20px;">Gottesdienste am {{ $dayServices[0]->day->date->format('d.m.Y') }} ({{ \App\Liturgy::getDayInfo($dayServices[0]->day)['title'] }})</h2>
+                @foreach($dayServices as $service)
+                    <div class="card-panel default registrable-service" id="{{ $randomId }}-{{ $loop->index }}">
+                        <h3>{{ $service->titleText(false) }}</h3>
+                        <table>
+                            <tr>
+                                <td valign="top">
+                                    {{ $service->day->date->format('d.m.Y') }}, {{ $service->timeText() }}<br/>
+                                    {{ $service->locationText() }}<br/><br/>
+                                    {{ $service->descriptionText() }}
+                                </td>
+                                <td valign="top" style="text-align: right;">
+                                    ca. {{ $service->getSeatFinder()->remainingCapacity() }} freie Plätze<br/>
+                                    <a class="btn btn-secondary show-reg-form" href="#"
+                                       data-container="#{{ $randomId }}-{{ $loop->index }}">Anmelden</a>
+                                </td>
+                            </tr>
+                        </table>
+                        <div class="reg-form" id="{{ $randomId }}-{{ $loop->index }}-reg" style="display: none;"
+                             data-service="{{ $service->id }}">
+                            <label for="name">Name</label>
+                            <input name="name" value="" type="text"/>
+                            <label for="first_name">Vorname</label>
+                            <input name="first_name" value="" type="text"/>
+                            <label for="street">Straße, Hausnummer</label>
+                            <input name="street" value="" type="text"/>
+                            <label for="zip">Postleitzahl</label>
+                            <input name="zip" value="" type="text"/>
+                            <label for="city">Ort</label>
+                            <input name="city" value="" type="text"/>
+                            <label for="phone">Telefonnummer</label>
+                            <input name="phone" value="" type="text"/>
+                            <label for="number">Anzahl Personen</label>
+                            <input name="number" value="1" type="text"/><br/><br/>
+                            <button class="btn btn-secondary submit-reg-form" href="#"
+                                    data-container="#{{ $randomId }}-{{ $loop->index }}">Anmeldung absenden
+                            </button>
+                            <br/>
+                            <small>Die Erhebung dieser Daten erfolgt nach $6 Abs. 1 CoronaVO. Nach §6 Abs. 4-5 CoronaVO
+                                können Sie nur
+                                nach korrekter Angabe Ihrer Kontaktdaten am Gottesdienst teilnehmen. Wir bitten hierfür
+                                um
+                                Verständnis.
+                            </small>
+                        </div>
+                    </div>
+                @endforeach
+            @endforeach
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" type="text/javascript"></script>
+    <script defer>
+        $(document).ready(function () {
+            $('.show-reg-form').click(function () {
+                $('.registrable-service').hide();
+                $($(this).data('container')).show();
+                $($(this).data('container') + '-reg').show();
+                $(this).hide();
+                $($(this).data('container') + '-reg input[name="name"]').focus();
+            });
+
+            $('.submit-reg-form').click(function () {
+                var url = '{{ $url }}&cities={{ join(',', $cities) }}';
+                var check = true;
+                ['name', 'first_name', 'street', 'zip', 'city', 'phone', 'number'].forEach(element => {
+                    if ($($(this).data('container') + '-reg input[name="' + element + '"]').val() == '') {
+                        alert('Das Feld "' + $('label[for="' + element + '"]').html() + '" darf nicht leer bleiben');
+                        check = false;
+                    }
+                });
+                if (check) {
+
+                    url += '&service=' + encodeURI($($(this).data('container') + '-reg').data('service'));
+
+                    ['name', 'first_name', 'number'].forEach(element => {
+                        if (element != 'contact') {
+                            url += '&' + element + '=' + encodeURI($($(this).data('container') + '-reg input[name="' + element + '"]').val())
+                        } else {
+                            url += '&' + element + '=' + encodeURI($($(this).data('container') + '-reg textarea[name="' + element + '"]').val())
+                        }
+                    });
+                    url += '&contact=' + encodeURI(
+                        $($(this).data('container') + '-reg input[name="street"]').val() + "\n"
+                        + $($(this).data('container') + '-reg input[name="zip"]').val() + " "
+                        + $($(this).data('container') + '-reg input[name="city"]').val() + "\n"
+                        + $($(this).data('container') + '-reg input[name="phone"]').val()
+                    );
+                    fetch(url)
+                        .then((res) => {
+                            return res.text();
+                        })
+                        .then((data) => {
+                            $('#{{ $randomId }}').html(data);
+                        });
+
+                }
+            });
+        });
+    </script>
+</div>
