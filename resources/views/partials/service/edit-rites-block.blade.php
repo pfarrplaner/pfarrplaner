b49e7c6162e87efaeed680670db162d4be4c2067
Christoph Fischer <chris@toph.de>
2020-11-21 19:59:46 +0100

    Add QR code button to homescreen

--- resources/views/partials/service/edit-rites-block.blade.php
+++ resources/views/partials/service/edit-rites-block.blade.php
@@ -5,7 +5,6 @@
             <a class="btn btn-sm btn-primary" href="{{route('services.edit', ['service' => $service->id, 'tab' => 'rites'])}}?back=/home" title="Gottesdienst bearbeiten"><span class="fa fa-edit"></span></a>
         @endcan
         @if(is_object($service->location) && count($service->location->seatingSections))
-            <br/>
             <div style="margin-top: 2px;">
                 <a class="btn btn-sm btn-success" href="{{ route('seatfinder', $service) }}" title="Neue Anmeldung">
                     <span class="fa fa-ticket-alt"></span>
@@ -16,6 +15,13 @@
                 <a class="btn btn-sm btn-secondary" href="{{ route('booking.finalize', $service) }}"
                    title="Sitzplatzliste drucken">
                     <span class="fa fa-clipboard-check"></span>
+                </a>
+            </div>
+        @endif
+        @if(\App\Integrations\KonfiApp\KonfiAppIntegration::isActive($service->city) && ($service->konfiapp_event_qr != ''))
+            <div style="margin-top: 2px;">
+                <a class="btn btn-sm btn-secondary" href="{{ route('report.step', ['report' => 'KonfiAppQR', 'step' => 'single', 'service' => $service->id]) }}" title="QR-Code drucken">
+                    <span class="fa fa-qrcode"></span>
                 </a>
             </div>
         @endif
