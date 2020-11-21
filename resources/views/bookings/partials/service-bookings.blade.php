f2cbf7e7115058a7b00ad7d57864738f7247e851
Christoph Fischer <chris@toph.de>
2020-11-21 15:54:56 +0100

    Show fixed-seat reservations in list

--- resources/views/bookings/partials/service-bookings.blade.php
+++ resources/views/bookings/partials/service-bookings.blade.php
@@ -15,7 +15,7 @@
                 <td>{{ $booking->name }}@if($booking->first_name), {{ $booking->first_name }}@endif</td>
                 <td>{{ $booking->number }}</td>
                 <td>{!!  nl2br($booking->contact) !!}</td>
-                <td>{{ $booking->code }}</td>
+                <td>{{ $booking->code }}@if($booking->fixed_seat)<br /><span style="color: red;"><small>Fester Platz: {{ $booking->fixed_seat }}</small></span>@endif</td>
                 <td class="text-right">
                     <a class="btn btn-sm btn-danger btn-delete-booking" title="Anmeldung löschen" data-route="{{ route('booking.destroy', $booking->id) }}" style="color: white;"><span class="fa fa-trash"></span></a>
                 </td>
