89da69dfbf3f74af8714b15eaeb9797154a59ca9
Christoph Fischer <chris@toph.de>
2020-12-03 07:38:44 +0100

    [FIX] locationselect remains empty on free-text locations

--- resources/views/partials/form/locationselect.blade.php
+++ resources/views/partials/form/locationselect.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group">
     <label for="{{ $name }}">{{ $label }}</label>
     <select class="form-control location-select @if(isset($class)){{ $class }}@endif" name="{{ $name }}" @if(isset($enabled) && (!$enabled)) disabled @endcannot >
-        @if(isset($value) && isset($special) && (null === $value) && ($special != ''))<option data-time="" value="{{ $special }}" selected>{{ $special }}</option>
+        @if(isset($special) && ($special != ''))<option data-time="" value="{{ $special }}" selected>{{ $special }}</option>
             @else <option></option>
         @endif
         @foreach($locations->sortBy('name') as $thisLocation)
