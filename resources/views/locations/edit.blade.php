60ff4888d5792d34c446f528c95d7eb8b9209f97
Christoph Fischer <chris@toph.de>
2020-12-11 20:15:48 +0100

    [FIX] locations.edit does not save

--- resources/views/locations/edit.blade.php
+++ resources/views/locations/edit.blade.php
@@ -6,7 +6,7 @@
     @component('components.container')
         @component('components.ui.card')
             @slot('cardFooter')
-                <button type="submit" class="btn btn-primary">Speichern</button>
+                <button id="form-location-edit-submit" type="submit" class="btn btn-primary">Speichern</button>
             @endslot
             @tabheaders
             @tabheader(['id' => 'home', 'title' => 'Allgemeines', 'active' => request()->get('tab', 'home') == 'home']) @endtabheader
@@ -14,14 +14,14 @@
             @endtabheaders
             @tabs
             @tab(['id' => 'home', 'active' => request()->get('tab', 'home') == 'home'])
-            <form method="post" action="{{ route('locations.update', $location->id) }}">
+            <form id="form-location-edit" method="post" action="{{ route('locations.update', $location->id) }}">
                 @method('PATCH')
                 @csrf
                 @input(['name' => 'name', 'label' => 'Name', 'value' => $location->name])
                 @select(['name' => 'city_id', 'label' => 'Kirchengemeinde', 'items' => $cities, 'value' => $location->city_id])
                 @input(['name' => 'default_time', 'label' => 'Gottesdienst um', 'value' => substr($location->default_time, 0, 5)])
                 @input(['name' => 'cc_default_location', 'label' => 'Wenn parallel Kinderkirche stattfindet, dann normalerweise hier', 'value' => $location->cc_default_location])
-                @select(['name' => 'alternate_location_id', 'label' => 'Wenn parallel Kinderkirche stattfindet, dann normalerweise hier', 'items' => $alternateLocations, 'value' => $location->alternate_location_id, 'empty' => true])
+                @selectize(['name' => 'alternate_location_id', 'label' => 'Wenn parallel Kinderkirche stattfindet, dann normalerweise hier', 'items' => $alternateLocations, 'value' => $location->alternate_location_id, 'empty' => true])
                 @input(['name' => 'at_text', 'label' => 'Ortsangabe, wenn ein Gottesdienst hier stattfindet', 'placeholder' => 'Ortsangabe, wenn ein Gottesdienst hier stattfindet', 'value' => $location->at_text])
                 @input(['name' => 'general_location_name', 'label' => 'Allgemeine Ortsangabe', 'placeholder' => 'z.B.: in Tailfingen', 'value' => $location->general_location_name])
             </form>
@@ -107,4 +107,14 @@
             @endtabs
         @endcomponent
     @endcomponent
+@endsection
+
+@section('scripts')
+    <script>
+        $(document).ready(function(){
+            $('#form-location-edit-submit').click(function(){
+                $('#form-location-edit').submit();
+            });
+        });
+    </script>
 @endsection
