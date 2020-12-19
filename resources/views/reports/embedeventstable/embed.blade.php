9e4933568de83d7f9c74c500c91e26a97e612ec9
Christoph Fischer <chris@toph.de>
2020-12-19 16:19:27 +0100

    Add streaming links and instructions to EmbedEventsTableReport

--- resources/views/reports/embedeventstable/embed.blade.php
+++ resources/views/reports/embedeventstable/embed.blade.php
@@ -1,11 +1,36 @@
-<style>
-    #{{ $randomId }} tr.hoverable:hover {
+<style id="buttonstyles">
+    .events-list-table tr.hoverable:hover {
         background-color: #B76FA5;
         cursor: pointer;
     }
+    .events-list-table td {
+        vertical-align: top;
+    }
+    .small-button {
+        font-size: .8em;
+        border: solid 1px darkgray;
+        padding: 4px 7px;
+        border-radius: 3px;
+        color: black;
+        cursor: pointer;
+    }
+    .youtube-button {
+        font-size: 1.2em;
+        border: solid 1px darkgray;
+        padding: 4px 7px;
+        border-radius: 3px;
+        cursor: pointer;
+    }
+    .youtube-button .fab {
+        color: red;
+    }
+    .youtube-button:hover, .small-button:hover {
+        background-color: white;
+        box-shadow: 0 5px 11px 0 rgba(0,0,0,0.18),0 4px 15px 0 rgba(0,0,0,0.15);
+    }
 </style>
 <div id="{{ $randomId }}">
-    <div id="{{ $randomId }}_table">
+    <div id="{{ $randomId }}_table" class="events-list-table">
         @if(count($events))
             <h3>Termine der nächsten {{ $days }} Tage</h3>
             <table class="ce-table striped">
@@ -49,9 +74,20 @@
                                         ({{ $event->participantsText('P') }})@endif
                                     @if($event->descriptionText())<br/>{{ $event->descriptionText() }}@endif
                                     @if ($event->offering_goal)<br/>Opfer: {{ $event->offering_goal }}@endif
+                                    <div>
+                                    @if ($event->songsheet) <span class="small-button" href="{{ $event->songsheetUrl }}" title="Klicken Sie hier, um das Liedblatt herunterzuladen"><span class="fa fa-file-pdf"></span> Liedblatt</span> @endif
+                                    @if ($event->offerings_url) <span class="small-button" href="{{ $event->offerings_url }}" title="Klicken Sie hier, um online zu spenden"><span class="fa fa-coins"></span> Opfer</span> @endif
+                                    @if ($event->cc_streaming_url) <span class="small-button" href="{{ $event->cc_streaming_url }}"title="Klicken Sie hier, um den Kindergottesdienst auf YouTube anzuschauen"><img src="{{ asset('img/cc.png') }}" height="12px"> Kinderkirche</span> @endif
+                                    @if ($event->external_url) <span class="small-button" href="{{ $event->external_url }}"  title="Klicken Sie hier, um zur Predigtseite zu gelangen"><span class="fa fa-globe"></span> Externe Seite zur Predigt</span> @endif
+                                    </div>
                                 </td>
                                 <td valign="top" style="vertical-align:top;">
-                                    @if (is_array($event)) @if(isset($event['place'])){{ $event['place'] }} @else @dd ($event) @endif @else {{ $event->locationText() }}@endif
+                                    @if (is_array($event)) @if(isset($event['place'])){{ $event['place'] }} @endif
+                                    @else {{ $event->locationText() }} @if($event->youtube_url)
+                                        <div><a class="youtube-button" target="_blank" href="{{ $event->youtube_url }}"><span class="fab fa-youtube"></span> Auf YouTube ansehen</a></div>
+                                    @endif @if (is_object($event->location) && ($event->location->instructions != ''))<div>
+                                                                           <small>{!! nl2br($event->location->instructions) !!}</small></div>@endif
+                                    @endif
                                 </td>
                             </tr>
                             @if ($event->cc)
@@ -84,6 +120,9 @@
     </div>
 </div>
 <script defer>
+    $('<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" />').appendTo("head");
+    $("<style type='text/css'>" + $('#buttonstyles').html() + "</style>").appendTo("head");
+
     function back_{{ $randomId }}(event) {
         event.preventDefault();
         $('#{{ $randomId }}_details').hide();
