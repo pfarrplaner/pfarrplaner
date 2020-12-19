cf8aa1b67ba35248153d8e4c8a3d60c1e8bc0700
Christoph Fischer <chris@toph.de>
2020-12-19 15:42:00 +0100

    Come back to previously active tab

--- resources/views/homescreen/configurable.blade.php
+++ resources/views/homescreen/configurable.blade.php
@@ -29,3 +29,35 @@
         @endif
     @endcomponent
 @endsection
+
+@section('scripts')
+<script>
+    if (location.hash) {
+        $('a[href=\'' + location.hash + '\']').tab('show');
+    }
+    var activeTab = localStorage.getItem('activeTab');
+    if (activeTab) {
+        $('a[href="' + activeTab + '"]').tab('show');
+    }
+
+    $('body').on('click', 'a[data-toggle=\'tab\']', function (e) {
+        e.preventDefault()
+        var tab_name = this.getAttribute('href')
+        if (history.pushState) {
+            history.pushState(null, null, tab_name)
+        }
+        else {
+            location.hash = tab_name
+        }
+        localStorage.setItem('activeTab', tab_name)
+
+        $(this).tab('show');
+        return false;
+    });
+    $(window).on('popstate', function () {
+        var anchor = location.hash ||
+            $('a[data-toggle=\'tab\']').first().attr('href');
+        $('a[href=\'' + anchor + '\']').tab('show');
+    });
+</script>
+@endsection
