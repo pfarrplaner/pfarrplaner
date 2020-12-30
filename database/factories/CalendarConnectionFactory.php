3693cfcf0191250230a337229cad07021fc28786
Christoph Fischer <chris@toph.de>
2020-12-30 10:26:10 +0100

    Commit missing files (factories and seeders)

---
+++ database/factories/CalendarConnectionFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\CalendarConnection;
+use Faker\Generator as Faker;
+
+$factory->define(CalendarConnection::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/SeatingRowFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\SeatingRow;
+use Faker\Generator as Faker;
+
+$factory->define(SeatingRow::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
