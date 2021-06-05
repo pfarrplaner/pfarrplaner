bad74b84fb6762883874ae7b7be057dcc274997f
Christoph Fischer <chris@toph.de>
2021-06-05 17:53:20 +0200

    fix: FormCheck does not handle 0 values correctly
    Now, FormCheck submits 0 as value if it's unchecked and initializes correctly when 0 (number) or "0" (string) is given as its value.

--- resources/js/components/Ui/forms/FormCheck.vue
+++ resources/js/components/Ui/forms/FormCheck.vue
@@ -29,8 +29,9 @@
 
 <template>
     <div class="form-check">
+        <input type="hidden" :name="name" value="0" />
         <input type="checkbox" class="form-check-input" :class="{'is-invalid': error}" :id="myId+'Input'" :name="name"
-               v-model="myValue" @input="$emit('input', $event.target.checked ? 1: 0)" value="1" :disabled="disabled"/>
+               :checked="myValue && (myValue != 0) && (myValue != '0')" @input="$emit('input', $event.target.checked ? 1: 0)" value="1" :disabled="disabled"/>
         <label class="form-check-label" v-if="label" :for="id+'Input'">{{ label }}</label>
         <span v-if="isCheckedItem" class="fa" :class="myValue ? 'fa-check-circle' : 'fa-times-circle'"></span>
         <div v-if="error" class="invalid-feedback">{{ error }}</div>
