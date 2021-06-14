140bce5ede5950928c2232ca5802b27d24db6825
Christoph Fischer <chris@toph.de>
2021-06-14 17:52:34 +0200

    fix: NextOfferingsTab is empty

--- resources/js/components/HomeScreen/NextOfferingsTab.vue
+++ resources/js/components/HomeScreen/NextOfferingsTab.vue
@@ -28,12 +28,54 @@
   -->
 
 <template>
+    <div class="next-offerings-tab">
+        <h2>{{ title }}</h2>
+        <p>Angezeigt werden die Gottesdienste der nächsten 2 Monate.</p>
+        <fake-table :columns="[2,4,4,2]" :headers="['Gottesdienst', 'Infos zum Gottesdienst', 'Opfer','']"
+                    collapsed-header="Gottesdienste">
+            <div v-for="(service,serviceIndex) in services" :key="serviceIndex"
+                 :class="{'stripe-odd': (serviceIndex %2 ==0)}" class="row mb-3 p-1">
+                <div class="col-md-2">
+                    <basic-info :service="service" />
+                </div>
+                <div class="col-md-4">
+                    <details-info :service="service" />
+                </div>
+                <div class="col-md-4">
+                    <div v-if="$can('gd-opfer-lesen') || $can('gd-opfer-bearbeiten')">
+                        <div>{{ service.offering_goal }}</div>
+                        <div v-if="service.offerings_counter1 || service.offerings_counter2" style="font-size: .8em;">
+                            <div>{{ service.offerings_counter1 }}</div>
+                            <div>{{ service.offerings_counter2 }}</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-2 text-right">
+                    <inertia-link class="btn btn-light" title="Im Kalender ansehen"
+                                  :href="route('calendar', moment(service.day.date).format('YYYY-MM'))">
+                        <span class="fa fa-calendar"></span>
+                    </inertia-link>
+                    <inertia-link class="btn btn-primary" title="Gottesdienst bearbeiten"
+                                  :href="route('services.edit', service.id)">
+                        <span class="fa fa-edit"></span>
+                    </inertia-link>
+                </div>
+            </div>
 
+        </fake-table>
+    </div>
 </template>
 
 <script>
+import BasicInfo from "../Service/BasicInfo";
+import FakeTable from "../Ui/FakeTable";
+import DetailsInfo from "../Service/DetailsInfo";
+import LiturgicalInfo from "../Service/LiturgicalInfo";
+
 export default {
-name: "NextOfferingsTab"
+    name: "NextOfferingsTab",
+    components: {BasicInfo, FakeTable, DetailsInfo, LiturgicalInfo},
+    props: ['title', 'description', 'user', 'settings', 'services']
 }
 </script>
 
