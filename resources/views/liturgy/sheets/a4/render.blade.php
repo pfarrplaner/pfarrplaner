bc1f2fa438c8d7b4ae4d6a21d353a60482aedd1e
Christoph Fischer <chris@toph.de>
2021-06-17 15:01:50 +0200

    refactor: Additional if/endif

--- resources/views/liturgy/sheets/a4/render.blade.php
+++ resources/views/liturgy/sheets/a4/render.blade.php
@@ -250,7 +250,7 @@
             <tr>
                 <td valign="top" width="25%" style="font-weight: bold">{{ $item->title }}</td>
                 @if($item->data_type == 'freetext')
-                    <td valign="top">{{ (false !== strpos($item->data['description'], "\n")) ? explode("\n", $item->data['description'])[0] : substr($item->data['description'], 0, 80).'...' }}</td>
+                    <td valign="top">@if($item->data['description']){{ (false !== strpos($item->data['description'], "\n")) ? explode("\n", $item->data['description'])[0] : substr($item->data['description'], 0, 80).'...' }}@endif</td>
                 @elseif($item->data_type == 'liturgic')
                     <td valign="top"></td>
                 @elseif($item->data_type == 'song')
