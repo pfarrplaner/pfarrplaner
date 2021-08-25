61b34fd13d287e3ed5d4b295f481168dcc5bf0d0
Christoph Fischer <chris@toph.de>
2021-08-25 10:27:28 +0200

    refactor: Remove obsolete blade templates for calendar

--- resources/views/calendar/partials/name.blade.php
+++
@@ -1,4 +0,0 @@
-@if($nameFormat==\App\User::NAME_FORMAT_DEFAULT){{ $participant->lastName(true) }}@endif
-@if($nameFormat==\App\User::NAME_FORMAT_INITIAL_AND_LAST){{ $participant->initialedName(true) }}@endif
-@if($nameFormat==\App\User::NAME_FORMAT_FIRST_AND_LAST){{ $participant->fullName(true) }}@endif
-
--- resources/views/calendar/partials/peoplelist.blade.php
+++
@@ -1,4 +0,0 @@
-@foreach($participants as $participant)
-    <span>@include('calendar.partials.name', ['person' => $participant])</span>
-    @if($loop->last) @else | @endif
-@endforeach
