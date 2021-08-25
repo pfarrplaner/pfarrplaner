94e4523343c6135bbe75883dd6653e221fbc653b
Christoph Fischer <chris@toph.de>
2021-08-25 15:43:20 +0200

    fix: Wrong redirection after login / auth routes

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -60,7 +60,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
