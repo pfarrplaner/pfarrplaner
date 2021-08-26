1520ba291340c6a99b9d0b957518f6c3e8a1569f
Christoph Fischer <chris@toph.de>
2021-08-26 17:09:22 +0200

    feat: Allow downloading existing file from FormImageAttacher

--- resources/js/components/Ui/forms/FormImageAttacher.vue
+++ resources/js/components/Ui/forms/FormImageAttacher.vue
@@ -34,6 +34,9 @@
         <div v-if="myValue">
             <img class="img-fluid" :src="route('image', myValue.replace('attachments/', ''))" />
             <div>
+                <a class="btn btn-light btn-sm" title="Bild herunterladen" :href="route('image', myValue.replace('attachments/', ''))">
+                    <span class="fa fa-download"></span> Herunterladen
+                </a>
                 <button class="btn btn-danger btn-sm" title="Bild entfernen" @click="detachImage"><span class="fa fa-trash"></span> Bild entfernen</button>
             </div>
         </div>
