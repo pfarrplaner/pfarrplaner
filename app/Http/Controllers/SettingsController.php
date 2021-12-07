e649aa7e608b48f4df04ceccd8c17bd9c5ae1c2a
Christoph Fischer <chris@toph.de>
2021-12-07 08:41:21 +0100

    feat: Handle inertia requests in SettingsController
    Signed-off-by: Christoph Fischer <chris@toph.de>

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -6,6 +6,7 @@ use App\Facades\Settings;
 use App\User;
 use App\UserSetting;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class SettingsController extends Controller
 {
@@ -19,9 +20,13 @@ class SettingsController extends Controller
         if ($user->id != \Auth::user()->id) {
             abort(403);
         }
-        $data = $request->validate(['value' => 'nullable|string']);
+        $data = $request->validate(['value' => 'nullable']);
         $userSetting = Settings::set($user, $key, $data['value']);
 
-        return response()->json($userSetting->only(['key', 'value']), 200);
+        if ($request->header('x-inertia')) {
+            return redirect()->back();
+        } else{
+            return response()->json($userSetting->only(['key', 'value']), 200);
+        }
     }
 }
