b7e47dd6cc48dbf947d47309df5eb286479f6cd5
Christoph Fischer <chris@toph.de>
2022-02-02 18:58:27 +0100

    fix: Edge recognizes site language as English
    Signed-off-by: Christoph Fischer <chris@toph.de>

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de" translate="no">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="google" content="notranslate">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -44,7 +45,7 @@
     <meta name="theme-color" content="#ffffff">
 </head>
 <body
-    class="hold-transition sidebar-mini sidebar-collapse {{ strtolower(str_replace('.', '-', Request::route()->getName())) }} @if(isset($slave) && $slave) slave @endif">
+    class="notranslate hold-transition sidebar-mini sidebar-collapse {{ strtolower(str_replace('.', '-', Request::route()->getName())) }} @if(isset($slave) && $slave) slave @endif">
     @inertia
 <!-- ./wrapper -->
 
