5139facc8d8f68d8467cdc8bf9333bb1edd31265
Christoph Fischer <chris@toph.de>
2022-02-08 08:31:03 +0100

    fix: Old Blade-rendered views show profile menu item twice
    Signed-off-by: Christoph Fischer <chris@toph.de>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -98,13 +98,6 @@
                     <nav class="mt-2">
                         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                             data-accordion="false">
-                            <li class="nav-item">
-                                <a href="{{ route('user.profile') }}"
-                                   class="nav-link @if(request()->is('user/profile')) active @endif">
-                                    <i class="nav-icon fa fa-user"></i>
-                                    <p>{{ Auth::user()->fullName() }}</p>
-                                </a>
-                            </li>
                             <div class="user-panel d-flex"></div>
                             <!-- Add icons to the links using the .nav-icon class
                                  with font-awesome or any other icon font library -->
