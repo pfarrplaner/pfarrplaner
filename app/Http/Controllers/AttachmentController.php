862fff116c13904d6b179be7d1d4e008a56f8d8f
Christoph Fischer <chris@toph.de>
2022-03-07 12:05:15 +0100

    feat: Let the user crop uploaded images
    Signed-off-by: Christoph Fischer <chris@toph.de>

---
+++ app/Http/Controllers/AttachmentController.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Pfarrplaner
+ *
+ * @package Pfarrplaner
+ * @author Christoph Fischer <chris@toph.de>
+ * @copyright (c) 2020 Christoph Fischer, https://christoph-fischer.org
+ * @license https://www.gnu.org/licenses/gpl-3.0.txt GPL 3.0 or later
+ * @link https://github.com/pfarrplaner/pfarrplaner
+ * @version git: $Id$
+ *
+ * Sponsored by: Evangelischer Kirchenbezirk Balingen, https://www.kirchenbezirk-balingen.de
+ *
+ * Pfarrplaner is based on the Laravel framework (https://laravel.com).
+ * This file may contain code created by Laravel's scaffolding functions.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace App\Http\Controllers;
+
+use App\Attachment;
+use App\City;
+use App\Integrations\KonfiApp\KonfiAppIntegration;
+use App\Integrations\Youtube\YoutubeIntegration;
+use App\Service;
+use App\Traits\HandlesAttachedImageTrait;
+use App\Traits\HandlesAttachmentsTrait;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+
+/**
+ * Class AttachmentController
+ * @package App\Http\Controllers
+ */
+class AttachmentController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
+    /**
+     * @param Request $request
+     * @param Attachment $attachment
+     */
+    public function update(Request $request, Attachment $attachment)
+    {
+        if ($request->hasFile('attachments')) {
+            $files = $request->file('attachments');
+            foreach ($files as $key => $file) {
+                $path = $file->store('attachments');
+                $attachment->update(['file' => $path]);
+            }
+        }
+        $attachment->refresh();
+        return response()->json($attachment->attachable->attachments);
+    }
+
+}
--- resources/js/components/Ui/forms/FormFileUploader.vue
+++ resources/js/components/Ui/forms/FormFileUploader.vue
@@ -31,18 +31,32 @@
     <div class="form-file-uploader">
         <div v-if="uploading">Datei wird hochgeladen... <span class="mdi mdi-spin mdi-loading"></span></div>
         <form-file-upload @input="upload" @upload-url="uploadUrl" multiple="1" />
+        <modal title="Bild zuschneiden" v-if="modalCropperOpen" min-height="50vh"
+               @close="cropImage" @cancel="modalCropperOpen = false;"
+               close-button-label="Zuschneiden" cancel-button-label="Original verwenden" max-width="800">
+            <cropper class="cropper" ref="cropper" :src="cropableImage" :canvas="canvasSettings" />
+        </modal>
     </div>
 </template>
 
 <script>
 import FormFileUpload from "./FormFileUpload";
+import Modal from "../modals/Modal";
+import { Cropper } from 'vue-advanced-cropper';
+import 'vue-advanced-cropper/dist/style.css';
+
+
 export default {
     name: "FormFileUploader",
-    components: {FormFileUpload},
-    props: ['parent', 'uploadRoute', 'title'],
+    components: {Modal, FormFileUpload, Cropper},
+    props: ['parent', 'uploadRoute', 'title', 'cropperCanvas'],
     data() {
         return {
             uploading: false,
+            modalCropperOpen: false,
+            cropableImage: '',
+            cropableAttachment: null,
+            canvasSettings: this.cropperCanvas || {},
         }
     },
     methods: {
@@ -53,7 +67,7 @@ export default {
                 title = file.name;
                 title = title.substr(0, title.lastIndexOf('.'));
                 title = title.charAt(0).toUpperCase() + title.slice(1);
-                title = window.prompt('Bitte gib eine Beschreibung für die Datei "'+fileName+'" an.', title);
+                title = window.prompt('Bitte gib eine Beschreibung für die Datei "' + fileName + '" an.', title);
             }
             if (null == title) return;
 
@@ -68,22 +82,52 @@ export default {
                     'Content-Type': 'multipart/form-data'
                 }
             }).then(response => {
-                this.$emit('input', response.data);
+                let attachments = response.data;
+                this.$emit('input', attachments);
                 this.uploading = false;
+                this.allowCropping(attachments);
             });
 
         },
         uploadUrl(data) {
             this.uploading = true;
-            axios.post(this.uploadRoute, { uploadFromUrl: data.url, attachment_text: data.description })
+            axios.post(this.uploadRoute, {uploadFromUrl: data.url, attachment_text: data.description})
                 .then(response => {
-                this.$emit('input', response.data);
-                this.uploading = false;
-            });
+                    let attachments = response.data;
+                    this.$emit('input', attachments);
+                    this.uploading = false;
+                    this.allowCropping(attachments);
+                });
+        },
+        allowCropping(attachments) {
+            if ((attachments.length > 0) && (attachments[attachments.length - 1].mimeType.substr(0, 6) == 'image/')) {
+                this.modalCropperOpen = true;
+                this.cropableImage = this.imageRoute(attachments[attachments.length - 1]);
+                this.cropableAttachment = attachments[attachments.length - 1];
+            }
+        },
+        imageRoute(attachment) {
+            return route('image', {path: attachment.file.replace('attachments/', '')});
+        },
+        cropImage() {
+            const {coordinates, canvas,} = this.$refs.cropper.getResult();
+            if (canvas) {
+                this.modalCropperOpen = false;
+                const form = new FormData();
+                canvas.toBlob(blob => {
+                    form.append('attachments[0]', blob);
+                    form.append('attachment_text[0]', this.cropableAttachment.title);
+                    this.uploading = true;
+                    axios.post(route('attachment.update', this.cropableAttachment.id), form)
+                        .then(response => {
+                            let attachments = response.data;
+                            this.$emit('input', attachments);
+                            this.uploading = false;
+                        });
+                }, 'image/jpeg');
+            }
         }
-
     }
-
 }
 </script>
 
