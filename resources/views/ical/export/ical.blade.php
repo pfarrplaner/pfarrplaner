029a322170c83aaf4d3d369e59b1878a5f42e830
Christoph Fischer <chris@toph.de>
2022-07-28 12:02:30 +0200

    fix: ICAL does not show newer services
    Signed-off-by: Christoph Fischer <chris@toph.de>

--- resources/views/ical/export/ical.blade.php
+++ resources/views/ical/export/ical.blade.php
@@ -2,7 +2,7 @@ BEGIN:VCALENDAR
 VERSION:2.0
 PRODID:{{ $calendarLink->getLink() }}
 METHOD:PUBLISH
-@foreach($data as $service)@if(is_object($service))@if(is_object($service->day))BEGIN:VEVENT
+@foreach($data as $service)@if(is_object($service))BEGIN:VEVENT
 UID:{{ $service->id }}{{ '@' }}{{ parse_url(env('APP_URL'), PHP_URL_HOST) }}
 LOCATION:{{ $service->locationText() }}
 SUMMARY:{{ wordwrap($service->titleText().' P: '.$service->participantsText('P').' O: '.$service->participantsText('O').' M: '.$service->participantsText('M').($service->description ? ' ('.$service->description.')' : ''), 64, "\r\n  ") }}
@@ -12,6 +12,6 @@ DTSTART:{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $service->date->forma
 DTEND:{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $service->date->format('Y-m-d').' '.$service->timeText(false).':00', 'Europe/Berlin')->addHour(1)->setTimezone('UTC')->format('Ymd\THis\Z') }}
 DTSTAMP:{{ $service->updated_at->setTimezone('UTC')->format('Ymd\THis\Z') }}
 END:VEVENT
-@endif @endif
+@endif
 @endforeach
 END:VCALENDAR
